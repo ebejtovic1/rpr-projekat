468f3caa1c201e4cd1e390cc9633428ac2bf3520
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2019-02-01 20:09:32 +0100

    Završena pretraga/filtriranje

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,20 +3,15 @@ package sample;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
 public class Controller {
@@ -68,9 +63,16 @@ public class Controller {
         title.setSortable(true);
         author.setSortable(true);
 
-        choiceBox.getItems().addAll("By title", "By author", "By field of study", "By publication type", "By year of issue");
-        choiceBox.setValue("By title");
-
+        choiceBox.getItems().addAll("All","By title", "By author", "By field of study", "By publication type", "By year of issue", "By ID", "By affiliation", "By citations", "By journal");
+        choiceBox.setValue("All");
+        choiceBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) ->
+        {
+            if (newVal != null)
+            {
+                textField.setText("");
+                flPerson.setPredicate(null);
+            }
+        });
 
         textField.setPromptText("Search here!");
         textField.setOnKeyReleased(keyEvent ->
@@ -80,33 +82,78 @@ public class Controller {
                 case "By title":
                     flPerson.setPredicate(p -> p.getTitle().toLowerCase().contains(textField.getText().toLowerCase().trim()));
                     break;
+
                 case "By author":
                     flPerson.setPredicate(p -> p.getAuthor().toLowerCase().contains(textField.getText().toLowerCase().trim()));
                     break;
+
                 case "By field of study":
                     flPerson.setPredicate(p -> p.getFieldOfStudy().getTitle().toLowerCase().contains(textField.getText().toLowerCase().trim()));
                     break;
+
                 case "By publication type":
                     flPerson.setPredicate(p -> p.getPublicationType().getType().toLowerCase().contains(textField.getText().toLowerCase().trim()));
                     break;
+
                 case "By year of issue":
                     flPerson.setPredicate(p -> String.valueOf(p.getYearOfIssue()).toLowerCase().contains(textField.getText().toLowerCase().trim()));
                     break;
 
+                case "By ID":
+                    flPerson.setPredicate(p -> String.valueOf(p.getId()).toLowerCase().contains(textField.getText().toLowerCase().trim()));
+                    break;
+
+                case "By affiliation":
+                    flPerson.setPredicate(p -> p.getAffiliation()!=null && p.getAffiliation().toLowerCase().contains(textField.getText().toLowerCase().trim()));
+                    break;
+
+                case "By citations":
+                    flPerson.setPredicate(p -> String.valueOf(p.getCitations()).toLowerCase().contains(textField.getText().toLowerCase().trim()));
+                    break;
+
+                case "By journal":
+                    flPerson.setPredicate(p -> p.getJournal()!=null && p.getJournal().toLowerCase().contains(textField.getText().toLowerCase().trim()));
+                    break;
+
+                case "All":
+                    flPerson.setPredicate(p -> {
+                        if(String.valueOf(p.getYearOfIssue()).toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        if(String.valueOf(p.getId()).toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        if(String.valueOf(p.getCitations()).toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        else if(p.getTitle().toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        else if( p.getAuthor().toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        else if(p.getFieldOfStudy().getTitle().toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        else if(p.getPublicationType().getType().toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        else if(p.getAffiliation()!=null && p.getAffiliation().toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+                        else if(p.getJournal()!=null && p.getJournal().toLowerCase().contains(textField.getText().toLowerCase().trim()))return true;
+
+                    return false;
+                    });
+                    break;
             }
         });
 
-       choiceBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) ->
-        {
-            if (newVal != null)
-            {
-                textField.setText("");
-                flPerson.setPredicate(null);
-            }
-        });
+
         SortedList<ScientificWork> sortedData = new SortedList<>(flPerson);
         sortedData.comparatorProperty().bind(table.comparatorProperty());
         table.setItems(sortedData);
+    }
+
+    public void print(ActionEvent actionEvent){
+
+    }
+    public void exit(ActionEvent actionEvent){
+        System.exit(0);
+    }
+    public void addS(ActionEvent actionEvent){
+
+    }
+
+    public void addF(ActionEvent actionEvent){
+
+    }
+    public void addP(ActionEvent actionEvent){
+
     }
 
 

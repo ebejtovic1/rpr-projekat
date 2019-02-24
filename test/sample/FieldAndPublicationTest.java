de0047f5b6ef36d942ea8ac17d08147478ada1ac
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2019-02-24 14:37:25 +0100

    Dodani testovi za Field, Publ i Scien klase

--- test/sample/FieldAndPublicationTest.java
+++ test/sample/FieldAndPublicationTest.java
@@ -25,6 +25,46 @@ class FieldAndPublicationTest {
 
     }
 
+    @Test
+    void getScien() {
+        ScientificWork field=new ScientificWork();
+        field.setId(649);
+        field.setAuthor("testic");
+        field.setJournal("Testic2");
+        field.setCitations(55);
+        field.setAffiliation("Afi");
+        field.setYearOfIssue(1998);
+        field.setTitle("Elma");
+        field.setFieldOfStudy(new FieldOfStudy(1435, "hehehhe"));
+        field.setPublicationType(new PublicationType(1234, "HEHE"));
+        assertEquals(649, field.getId());
+        assertEquals("testic", field.getAuthor());
+        assertEquals("Testic2",field.getJournal());
+        assertEquals(55,field.getCitations());
+        assertEquals("Afi",field.getAffiliation());
+        assertEquals("hehehhe",field.getFieldOfStudy().getTitle());
+        assertEquals("HEHE",field.getPublicationType().getType());
+        assertEquals(1998, field.getYearOfIssue());
+        assertEquals("Elma",field.getTitle());
+
+
+    }
+    @Test
+    void getScien1() {
+        ScientificWork field=new ScientificWork(649,"Elma","testic",new FieldOfStudy(1435, "hehehhe"),"Testic2",new PublicationType(1234, "HEHE"),1998,55,"Afi");
+        assertEquals(649, field.getId());
+        assertEquals("testic", field.getAuthor());
+        assertEquals("Testic2",field.getJournal());
+        assertEquals(55,field.getCitations());
+        assertEquals("Afi",field.getAffiliation());
+        assertEquals("hehehhe",field.getFieldOfStudy().getTitle());
+        assertEquals("HEHE",field.getPublicationType().getType());
+        assertEquals(1998, field.getYearOfIssue());
+        assertEquals("Elma",field.getTitle());
+
+
+    }
+
 
 
 }
